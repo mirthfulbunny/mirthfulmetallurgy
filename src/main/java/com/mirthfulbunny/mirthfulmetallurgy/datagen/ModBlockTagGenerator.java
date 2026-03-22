387772b6e985b6d0ad2be033a6fa8ca366f6202e
EnternodeCS <20007583j@gmail.com>
2026-03-22 15:36:51 -0400

    Bloomery and Hammer Table Mineable w/ Pickaxe

--- src/main/java/com/mirthfulbunny/mirthfulmetallurgy/datagen/ModBlockTagGenerator.java
+++ src/main/java/com/mirthfulbunny/mirthfulmetallurgy/datagen/ModBlockTagGenerator.java
@@ -1,10 +1,13 @@
 package com.mirthfulbunny.mirthfulmetallurgy.datagen;
 
 import com.mirthfulbunny.mirthfulmetallurgy.MirthfulMetallurgy;
+import com.mirthfulbunny.mirthfulmetallurgy.block.ModBlocks;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
+import net.minecraft.tags.BlockTags;
 import net.minecraftforge.common.data.BlockTagsProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.concurrent.CompletableFuture;
@@ -15,7 +18,10 @@ public class ModBlockTagGenerator extends BlockTagsProvider {
     }
 
     @Override
-    protected void addTags(HolderLookup.Provider provider) {
-
+    protected void addTags(HolderLookup.@NotNull Provider provider) {
+        this.tag(BlockTags.MINEABLE_WITH_PICKAXE).add(
+                ModBlocks.BLOOMERY.get(),
+                ModBlocks.HAMMER_TABLE.get()
+        );
     }
 }
